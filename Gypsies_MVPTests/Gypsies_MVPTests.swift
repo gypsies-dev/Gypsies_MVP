246b079588c909e54579db72241ce442de8559fd
tomoki0307 <goosehouse.sekaowa.melody@gmail.com>
2020-01-09 17:48:00 +0900

    first commit

---
+++ Gypsies_MVPTests/Gypsies_MVPTests.swift
@@ -0,0 +1,34 @@
+//
+//  Gypsies_MVPTests.swift
+//  Gypsies_MVPTests
+//
+//  Created by 筒井知生 on 2020/01/09.
+//  Copyright © 2020 筒井知生. All rights reserved.
+//
+
+import XCTest
+@testable import Gypsies_MVP
+
+class Gypsies_MVPTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
